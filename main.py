4e94a8df4f7f6112cafb215e8b694d74f28d8ac7
Шаров Глеб <glscharow@yandex.ru>
2023-12-07 19:55:25 +0000

    Finished syracuse_sequence

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from typing import List
+
+
 def main() -> None:
     """Main function"""
     pass
+
+
+def syracuse_sequence(n: int) -> List[int]:
+    sequence = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = n * 3 + 1
+        sequence.append(n)
+    return sequence
 
 
 if __name__ == '__main__':
