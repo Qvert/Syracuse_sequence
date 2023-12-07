d91c8cda9b934b8aea65096d49f51e334460ccf9
Шаров Глеб <glscharow@yandex.ru>
2023-12-07 20:01:44 +0000

    Finished function syracuse_max

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from typing import List
 
 
-def main() -> None:
-    """Main function"""
-    pass
-
-
 def syracuse_sequence(n: int) -> List[int]:
     sequence = [n]
     while n != 1:
@@ -15,6 +10,20 @@ def syracuse_sequence(n: int) -> List[int]:
             n = n * 3 + 1
         sequence.append(n)
     return sequence
+
+
+def syracuse_max(n: int) -> int:
+    sequence = syracuse_sequence(n)
+    max_num = n
+    for max_value in sequence:
+        if max_value > max_num:
+            max_num = max_value
+    return max_num
+
+
+def main() -> None:
+    """Main function"""
+    print(syracuse_max(int(input("Введите число: "))))
 
 
 if __name__ == '__main__':
